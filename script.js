e94fd8d79b44ccc0005f2c3c943bdad4448f2e1d
Gabriel <gabriel_v_alves@estudante.sesisenai.org.br>
2025-03-20 10:03:06 -0300

    ate a 4

--- script.js
+++ script.js
@@ -1,2 +1,54 @@
-console.log("hello world")
 
+console.log('hello world');  
+
+
+let num = prompt ('digite um numero')
+function verificarPar(num) {  
+    
+    if (num % 2 === 0) {  
+        console.log('par');  
+    } else {  
+        console.log('impar');  
+    }  
+}  
+
+ 
+verificarPar();  
+
+
+let num1 = parseFloat (prompt (' digite o primeiro numero'));
+let num2 = parseFloat (prompt (' digite o segundo numero'));
+
+function calcular(num1, num2){
+
+   
+   
+console.log('soma'+(num1 + num2))
+console.log('subtração'+(num1 - num2))
+console.log('multiplicação'+(num1 * num2))
+console.log('divisão'+(num1 / num2))
+
+}
+
+calcular(num1, num2);
+
+
+let n = 10;
+
+while (n > 1){
+   console.log(n--) 
+}
+console.log(n);
+
+
+
+const Original = prompt('digite uma palavra')
+
+function inverterTexto(palavra) {  
+    return palavra.split('').reverse().join('');  
+}  
+
+
+const Invertida = inverterTexto(Original);  
+
+console.log(Invertida);
